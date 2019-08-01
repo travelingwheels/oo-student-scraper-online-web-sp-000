a3c621ac0ab146335ee4976b5f99c7def4895835
Gary Wheeler <gkwheeler40@gmail.com>
2019-08-01 14:04:17 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -18,7 +18,7 @@ class Student
   def add_student_attributes(attributes_hash)
     attributes_hash.each {|key, value| self.send("#{key}=", value)}
   end
-   binding.pry
+  
   def self.all
     @@all
   end
